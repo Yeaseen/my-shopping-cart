5c94633e60cbf151882a40f3395f50794dae1abb
Yeaseen <yeaseen.arafat96@gmail.com>
2025-09-10 00:03:09 -0600

    backward compatible npm start and +session storage

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,14 @@
-import React, { useEffect } from 'react'
-import { Link, useHistory } from 'react-router-dom'
-
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
 const NavBar = () => {
-    const history = useHistory()
-
-    useEffect(()=>{
-        {history.push("/")} 
-    }, [])
-
-    return (
-        <div className="norm">
-            <div className="nav-wrapper white">
-                <Link to="/" className="cus ">Shopping Cart</Link>
-                
-            </div>
-        </div>
-    )
+  return (
+    <div className="norm">
+      <div className="nav-wrapper white">
+        <Link to="/" className="cus">Shopping Cart</Link>
+      </div>
+    </div>
+  )
 }
 
 export default NavBar
